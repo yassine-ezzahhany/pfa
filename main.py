9781c7143e3cae2bfe2579200012e9ffd5ba0830
EZZAHHANY Yassine <ezzahaniyassine@gmail.com>
2025-12-22 01:58:39 +0100

    autoriser l'acces a tout le monde

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.register_router import register_router 
 #Le serveur:
 app=FastAPI(title="PFA APIs")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # tout le monde peut accéder
+    allow_credentials=True,  # pas de cookies
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 #liee les routes:
 app.include_router(router=register_router, prefix="/register", tags=["Enregistrement"])
