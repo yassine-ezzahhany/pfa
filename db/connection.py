05587fc33fe47bea12d363be4bc08a6b24be30ed
EZZAHHANY Yassine <ezzahaniyassine@gmail.com>
2026-02-05 13:17:03 +0100

    fix db problem

--- db/connection.py
+++ db/connection.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()  # charge le .env
 mongo_uri = os.getenv("MONGO_URI")
 client = MongoClient(mongo_uri)
-db = client.get_database()  # accède à pfa_db
+db = client.get_database("pfa_db")    # accède à pfa_db
